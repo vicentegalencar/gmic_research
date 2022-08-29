837784dbe92dbae340c4db0079db32661372baa7
filipe@aiml.team <filipe@aiml.team>
2022-08-29 17:24:17 -0300

    model

--- Global_local_framework_2/src/scripts/one_epoch.py
+++ Global_local_framework_2/src/scripts/one_epoch.py
@@ -170,7 +170,7 @@ def running_one_epoch_inbreast(epoch, output_path, model, train_loader, val_load
     print('{} Epoch finished ! Train Loss: {}, lr: {}, train acc:{}, train roc_auc:{}'.format(epoch+1, epoch_loss, format(lr,'.2e'), acc, roc_auc))
     train_loss.append(epoch_loss)
     train_acc.append(acc)
-    val_loss, val_acc, val_roc_auc, val_TP, val_FN, val_TN, val_FP = val_epoch(epoch, output_path, model, val_loader, criterion, device, threshold, \
+    val_loss, val_acc, val_roc_auc, val_TP, val_FN, val_TN, val_FP = val_epoch_inbreast(epoch, output_path, model, val_loader, criterion, device, threshold, \
         beta, valid_loss, valid_acc, cam_method)
     val_sen = val_TP/(val_TP+val_FN)
     val_spe = val_TN/(val_TN+val_FP)
@@ -282,6 +282,61 @@ def val_epoch(epoch, output_path, model, loader, criterion, device, threshold, b
 
     print('')
     return epoch_loss, acc, roc_auc, TP, FN, TN, FP
+
+@torch.no_grad()
+def val_epoch_inbreast(epoch, output_path, model, loader, criterion, device, threshold, beta, valid_loss, valid_acc, cam_method):
+    global val_best_roc_auc
+    model.eval()
+    epoch_loss = 0
+    prediction = np.empty(shape=[0, 2], dtype=np.int)
+    for step, (imgs, labels, _) in enumerate(loader):
+        imgs, labels = imgs.to(device), labels.to(device)
+        
+        # if cam_method == "cam":
+        y_global, y_local, y_fusion, saliency_map, _, _ = model(imgs)
+        # elif cam_method == "xgradcam":
+        #     h_g, saliency_map, cam_score = model.forward_global(imgs)
+        #     y_global, y_local, y_fusion, saliency_map, _, _ = model.altercam(imgs,h_g, cam_score,activation='xgradcam')
+        #     #y_global, y_local, y_fusion, saliency_map, _, _ = model.forward_altercam(imgs,activation='xgradcam')
+        
+        y_global = y_global[:,1:]
+        y_local = y_local[:,1:]
+        y_fusion = y_fusion[:,1:]
+        saliency_map = saliency_map[:,1:,:,:]
+
+        loss1 = criterion(y_global, labels)
+        loss2 = criterion(y_local, labels)
+        loss3 = criterion(y_fusion, labels)
+        loss4 = beta * saliency_map.mean()
+        loss =  loss1 + loss2 + loss3 + loss4
+        epoch_loss += loss.item()
+
+        result = np.concatenate([y_fusion.cpu().data.numpy(), labels.cpu().data.numpy()], axis=1)
+        prediction = np.concatenate([prediction, result], axis=0)
+
+    epoch_loss /= step
+    print('==> ### validate metric ###')
+    print('Epoch: {} --- Val Loss: {}'.format(epoch+1, epoch_loss))
+    print('loss_global: {0:.4f} --- loss_local: {1:.4f} --- loss_fusion: {2:.4f}'.format(loss1, loss2, loss3))
+
+    # compute metric
+    total = len(loader.dataset)
+    TP, FN, TN, FP, acc, roc_auc = compute_metric(prediction, threshold)
+    print('threshold: %.2f --- TP: %d --- FN: %d --- TN: %d --- FP: %d'%(threshold, TP, FN, TN, FP))
+    print('acc: %f --- roc_auc: %f'%(acc, roc_auc))
+    print('Total: %d --- cur_best_val_roc_auc: %f'%(total, val_best_roc_auc))
+    valid_loss.append(epoch_loss)
+    valid_acc.append(acc)
+
+    # save best
+    if roc_auc > val_best_roc_auc:
+        val_best_roc_auc = roc_auc
+        save_epoch(epoch, output_path, model)
+        print('Save New Best Successfully at epoch %d, valid_acc %.2f, valid_auc %.2f'%(epoch+1, acc, roc_auc))
+
+    print('')
+    return epoch_loss, acc, roc_auc, TP, FN, TN, FP
+
 
 @torch.no_grad()
 def val_epoch_global_only(epoch, output_path, model, loader, criterion, device, threshold, beta, valid_loss, valid_acc):
