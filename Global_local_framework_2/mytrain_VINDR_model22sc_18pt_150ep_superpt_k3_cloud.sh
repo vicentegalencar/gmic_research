ced3cc7141856df1a202f88845eda98d75b2b66f
filipe@aiml.team <filipe@aiml.team>
2022-09-01 14:30:09 -0300

    modelcloud

--- Global_local_framework_2/mytrain_VINDR_model22sc_18pt_150ep_superpt_k3_cloud.sh
+++ Global_local_framework_2/mytrain_VINDR_model22sc_18pt_150ep_superpt_k3_cloud.sh
@@ -11,7 +11,7 @@ export PYTHONPATH=$PYTHONPATH:"/content/gmic_research/Global_local_framework_2/"
 
 python src/scripts/mytrain_vindr_superpt.py \
 --save_model_path="checkpoints/vindr_rn22pt_rn18pt_ep50_superpt_k3_cam" \
---data_path="/media/hdd/filipe/datasets/vindr-mammo_small/" \
+--data_path="/content/drive/MyDrive/vindr-mammo_small/" \
 --epochs=50 \
 --lr=4.134478662168656e-05 \
 --lr_step=10 \
